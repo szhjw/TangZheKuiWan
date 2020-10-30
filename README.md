cf77da6d9b3959c8762a3510454fc3184c007108
fc01 <910677221@qq.com>
2020-10-30 16:37:41 +0800

    ---

---
+++ README.md
@@ -0,0 +1,15 @@
+# TangZheKuiWan
+<img src="1.png">
+
+
+# 安装依赖
+npm i  
+
+# 监控编译
+npm run w  
+
+# 运行服务器
+npm run server   
+
+# 运行客户端
+npm start
---
+++ src/userConfig.ts
@@ -0,0 +1,17 @@
+import { ConfigType } from './ConfigType'
+
+export const userConfig: ConfigType = {
+    ssPort: 1080,
+    ss: true,
+    下单ss: false,
+    提醒音量: 1,
+    单位时间: 500,
+    account: {
+        'aaa': {
+            cookie: '',
+            下单数量: 1,
+            orderServerIP: '127.0.0.1',
+        },
+       
+    }
+} 
